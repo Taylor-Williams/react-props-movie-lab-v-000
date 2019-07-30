76de83c7c9364fe7cf4cd0dc44dae8dc7874b38c
Taylor Williams <taylor@thewilliams.net>
2019-07-30 00:44:11 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -41,3 +41,8 @@ export default class MovieCard extends Component {
 }
 
 // Don't forget your default props!
+const MovieCard.defaultProps = {
+  title : "unknown",
+  rating : null,
+  poster : "default"
+} 
