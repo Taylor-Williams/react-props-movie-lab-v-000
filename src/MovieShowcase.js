78fd3d14aa6fb9efdd5ad18bdeac7d822da8ceb5
Taylor Williams <taylor@thewilliams.net>
2019-07-30 00:37:20 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -8,7 +8,7 @@ export default class MovieShowcase extends Component {
     // map over your movieData array and return the correct
     return (
       <div className='movie-showcase'>
-        {movieData.map((movie) => <MovieCard movie={movie} />}
+        {movieData.map((movie) => <MovieCard title={movie.title} rating={movie.IMDBRating} genresarray={movie.genres} poster={movie.poster} />}
       </div>
     )
   }
